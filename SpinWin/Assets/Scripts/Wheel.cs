4b7729fa3e148258b034b6fe8d01261cadeef0e6
Mayank Gaba <cu.17bcs1192@gmail.com>
2020-06-09 15:45:44 +0530

    done with the programming of Wheel

--- SpinWin/Assets/Scripts/Wheel.cs
+++ SpinWin/Assets/Scripts/Wheel.cs
@@ -1,22 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Wheel : MonoBehaviour
 {
-    public Vector3 rotationDirection;
-    public float durationTime;
-    //private float convertedTime = 200;
-    private float smooth;
-    // Start is called before the first frame update
+    int rand;
+    private bool spinAllowed;
+    private float timeInterval;
+    private float totalAngle;
+    private float finalAngle;
+
     void Start()
-    {
-        
+    {   
+        spinAllowed = true;
+        totalAngle = 360/12;
     }
-    // Update is called once per frame
     void Update()
     {
-        smooth = Time.deltaTime * durationTime;
-        transform.Rotate(rotationDirection * smooth);
+        if(Input.GetMouseButton(0) && spinAllowed)
+            StartCoroutine(Spin());
+    }
+
+    private IEnumerator Spin()
+    {
+        spinAllowed = false;
+        rand = Random.Range(20,30);
+        timeInterval = 0.1f;
+
+        for(int i=0;i<rand;i++)
+        {
+                transform.Rotate(0,0,totalAngle/2);
+                 if (i > Mathf.RoundToInt (rand * 0.5f))timeInterval = 0.2f;
+                 if (i > Mathf.RoundToInt (rand * 0.85f))timeInterval = 0.4f;
+                 yield return new WaitForSeconds (timeInterval);
+        }
+
+        if(Mathf.RoundToInt (transform.eulerAngles.z) % 45 != 0)transform.Rotate (0, 0, 22.5f);
+        finalAngle = Mathf.RoundToInt (transform.eulerAngles.z);
+        
+        spinAllowed = true;
     }
 }
