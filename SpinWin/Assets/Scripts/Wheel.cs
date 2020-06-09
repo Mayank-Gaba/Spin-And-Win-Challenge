2df0fbe6778a98caff64c0332aab2f002e360f59
Mayank Gaba <cu.17bcs1192@gmail.com>
2020-06-09 20:01:30 +0530

    Completed the Project

--- SpinWin/Assets/Scripts/Wheel.cs
+++ SpinWin/Assets/Scripts/Wheel.cs
@@ -9,7 +9,6 @@ public class Wheel : MonoBehaviour
     private bool spinAllowed;
     private float timeInterval;
     private float totalAngle;
-    private float finalAngle;
 
     void Start()
     {   
@@ -18,27 +17,29 @@ public class Wheel : MonoBehaviour
     }
     void Update()
     {
-        if(Input.GetMouseButton(0) && spinAllowed)
+        if(Input.GetKeyDown("space") && spinAllowed)
             StartCoroutine(Spin());
     }
 
     private IEnumerator Spin()
     {
         spinAllowed = false;
-        rand = Random.Range(20,30);
-        timeInterval = 0.1f;
+        rand = Random.Range(250,300);
+        timeInterval = 0.0001f*2*Time.deltaTime;
 
         for(int i=0;i<rand;i++)
         {
                 transform.Rotate(0,0,totalAngle/2);
-                 if (i > Mathf.RoundToInt (rand * 0.5f))timeInterval = 0.2f;
-                 if (i > Mathf.RoundToInt (rand * 0.85f))timeInterval = 0.4f;
-                 yield return new WaitForSeconds (timeInterval);
+                if(i > Mathf.RoundToInt (rand * 0.2f))timeInterval = 0.5f*Time.deltaTime;
+                if(i > Mathf.RoundToInt (rand * 0.3f))timeInterval = 1.0f*Time.deltaTime;
+                if(i > Mathf.RoundToInt (rand * 0.5f))timeInterval = 1.5f*Time.deltaTime;
+                if(i > Mathf.RoundToInt (rand * 0.8f))timeInterval = 2.0f*Time.deltaTime;
+                if(i > Mathf.RoundToInt (rand * 0.9f))timeInterval = 2.5f*Time.deltaTime;
+                
+                yield return new WaitForSeconds (timeInterval);
         }
 
-        if(Mathf.RoundToInt (transform.eulerAngles.z) % 45 != 0)transform.Rotate (0, 0, 22.5f);
-        finalAngle = Mathf.RoundToInt (transform.eulerAngles.z);
-        
+        if(Mathf.RoundToInt (transform.eulerAngles.z) % 30 != 0)transform.Rotate (0, 0,21f);        
         spinAllowed = true;
     }
 }
